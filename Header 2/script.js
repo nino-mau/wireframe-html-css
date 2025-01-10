6bdeeb0000b18e90a55f4194f9179c140e23436e
nino.mauuary <nino.mauuary@gmail.com>
2025-01-10 17:08:30 +0100

    Copy project from local

---
+++ Header 2/script.js
@@ -0,0 +1,24 @@
+/** Scripts for sliding menu */
+
+const menu = document.getElementById("menu_bar");
+const checkbox = document.getElementById("menu_toggle");
+const main = document.getElementById("content");
+
+function handleSlidingMenu() {
+    if (checkbox.checked === true) {
+        menu.style.left = "0vw";
+        main.style.marginLeft = "18vw";
+        console.log("Menu shown");
+    }
+    else {
+        menu.style.left = "-18vw";
+        main.style.marginLeft = "0vw";
+        console.log("Menu hidden");
+    }
+}
+
+function checkboxEventListener() {
+    checkbox.addEventListener('click', handleSlidingMenu);
+}
+
+checkboxEventListener();
