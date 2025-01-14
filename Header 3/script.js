963f31bdcd21c2837135f0a64fb9df94b7fef1f0
Nino Mauuary <nino.mauuary@gmail.com>
2025-01-14 16:07:47 +0100

    started working on adding slick menu

---
+++ Header 3/script.js
@@ -0,0 +1,11 @@
+
+/** -- FOR SlICK MENU -- */
+
+$(document).ready(function(){
+    // Slick preset
+    $('.gallery-slick-menu').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
